49a25a46129aafb7250ca2fcee4c2a22a94e7a8b
ArneMartin <arnemartinp@gmail.com>
2017-08-29 20:57:25 +0200

    Pathfinding
    updated steamvr plugin to version 1.2.2
    +added navmesh to scene lvl1
    +added simple spawner with timer
    +added test enemy in /models with pathfinding
    +added goal in /models
    +added pathfinding folder in scripts
    +added script for pathfinding
    +added script for spawner

---
+++ Assets/Scripts/Pathfinding/MoveTo.cs
@@ -0,0 +1,13 @@
+﻿ // MoveTo.cs
+using UnityEngine;
+using System.Collections;
+
+public class MoveTo : MonoBehaviour {
+
+	public Transform goal;
+
+	void Start () {
+		UnityEngine.AI.NavMeshAgent agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+		agent.destination = goal.position; 
+	}
+}
---
+++ Assets/Scripts/Pathfinding/spawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour {
+	public GameObject penguin;
+	private Vector3 pos= new Vector3(-1f,2.88f,-16.33f);
+	private int a;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		a++;
+		if (a > 120) {
+			a = 0;
+			Instantiate (penguin, pos, Quaternion.identity);
+		}
+	}
+}

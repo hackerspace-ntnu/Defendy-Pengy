a52b4f2d7523e00e760afeb1128bba16777b76db
Hackerspace Vive <hackerspace@hackerspace>
2017-08-31 20:56:58 +0200

    Got laser teleportation, grabbing and longbow working. Added some models.

---
+++ Assets/IDamagable.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDamagable {
+	float GetHealth ();
+	float DecreaseHealth ();
+}
---
+++ Assets/Scripts/FloatOnWaterStatic.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatOnWaterStatic : MonoBehaviour {
+	public float moveAmount = 1f;
+	private float startTime = 0f;
+	private float originalPosY;
+	// Use this for initialization
+	void Start () {
+		originalPosY = transform.position.y;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (transform.position.x,originalPosY + Mathf.Sin (Time.time)*0.04f*moveAmount,transform.position.z);
+	}
+}

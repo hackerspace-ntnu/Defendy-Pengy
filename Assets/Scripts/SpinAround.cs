82fc72e0a06ba5c727f92c790923a88a0f0ec292
Håkon Vågsether <haakonsv@gmail.com>
2017-09-14 20:34:35 +0200

    Fixed SpinAround's class name

--- Assets/Scripts/SpinAround.cs
+++ Assets/Scripts/SpinAround.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveAround : MonoBehaviour {
+public class SpinAround : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {

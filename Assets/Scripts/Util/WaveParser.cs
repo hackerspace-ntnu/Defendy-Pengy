233009c440d2a27d0e4826d9bf1e9ad40b99ce84
Håkon Vågsether <hauk142@gmail.com>
2017-10-05 21:00:24 +0200

    Sprint 3 (#12)
    * Added Sound FX
    
    * Add Steam Audio package
    
    * Tag SteamAudio geometry and materials, and pre-export scene
    
    * Add files via upload
    
    * Moved starting point to tower
    
    * lvl 1 muzak
    
    * Removed lamp, sphere and armature from fox
    
    * Removed camera, lamp, etc. from trees
    
    * Added WaveParser
    
    * Added enemy lvl2 seal
    
    * Added lvl2 enemy seal to Models
    
    * Add files via upload
    
    * Deleted corrupted Level 1 map
    
    * Add files via upload
    
    * Add files via upload
    
    * Removed new path .fbx file (unneeded)
    
    * Resized level, repositioned towers, added mesh colliders
    
    * Fixed positioning in the towers when teleporting
    
    * Removed bow pedestal
    
    * Renamed objects
    
    * Removed lamp and camera from gress_straw
    
    * Add files via upload
    
    * Added small gress_straw
    
    * Added gran_tree_improved prefab
    
    * Added mesh collider to gran_tree prefab
    
    * Added foliage and decoration prefabs
    
    * Updated decoration prefabs
    
    * Changed wave configuration
    
    * Replaced enemy placeholder with wolf
    
    * Added Level 1 music
    
    * Added collider to enemy wolf

---
+++ Assets/Scripts/Util/WaveParser.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+public class WaveParser
+{
+	public static List<EnemyWave> ParseWaveFile(string filePath)
+	{
+		List<EnemyWave> waves = new List<EnemyWave>();
+
+		List<EnemySpawner.EnemyType> enemies_currentWave = new List<EnemySpawner.EnemyType>();
+		List<float> delays_currentWave = new List<float>();
+
+		foreach (string l in File.ReadAllLines(filePath))
+		{
+			string line = l.Trim();
+			if (line.Length == 0 || line[0] == '/' || line[0] == '#')
+			{
+				AddWave(waves, enemies_currentWave, delays_currentWave);
+				continue;
+			}
+
+			string[] tokens = line.Split(' ');
+			switch (tokens.Length)
+			{
+				case 0:
+					break;
+
+				case 2:
+					enemies_currentWave.Add(GetEnemyType(tokens[0]));
+					delays_currentWave.Add(float.Parse(tokens[1]));
+					break;
+
+				case 3:
+					int enemyCount = int.Parse(tokens[0]);
+					EnemySpawner.EnemyType enemy = GetEnemyType(tokens[1]);
+					float delay = float.Parse(tokens[2]);
+					for (int i = 0; i < enemyCount; i++)
+					{
+						enemies_currentWave.Add(enemy);
+						delays_currentWave.Add(delay);
+					}
+					break;
+
+				default:
+					throw new System.FormatException("Invalid enemy type declaration: " + line);
+			}
+		}
+
+		if (enemies_currentWave.Count > 0)
+			AddWave(waves, enemies_currentWave, delays_currentWave);
+
+		return waves;
+	}
+
+	private static void AddWave(List<EnemyWave> waves, List<EnemySpawner.EnemyType> enemies_currentWave, List<float> delays_currentWave)
+	{
+		if (enemies_currentWave.Count == 0)
+			return;
+
+		waves.Add(new EnemyWave(enemies_currentWave.ToArray(), delays_currentWave.ToArray()));
+		enemies_currentWave.Clear();
+		delays_currentWave.Clear();
+	}
+
+	private static EnemySpawner.EnemyType GetEnemyType(string token)
+	{
+		switch (token)
+		{
+			case "wolf":
+				return EnemySpawner.EnemyType.Wolf;
+
+			default:
+				throw new System.FormatException("Invalid enemy type: " + token);
+		}
+	}
+}

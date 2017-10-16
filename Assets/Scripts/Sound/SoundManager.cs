d2e2d93c637b6bc5c5ecd50cc68ee3a79686ff61
Dabble <andershovden@protonmail.com>
2017-10-17 00:27:20 +0200

    Play point sound through SoundManager
    Updated SoundManager with PlayRandomSound() (from Enemy_Sound) and PlayRandomSoundAtPoint(), together with a PointSound prefab.

--- Assets/Scripts/Sound/SoundManager.cs
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -4,20 +4,47 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-	public Transform enemyContainer;
-	public Transform sound;
+	private static SoundManager INSTANCE;
+
+	public Transform pointSoundPrefab;
 
 	void Start()
 	{
-
+		INSTANCE = this;
 	}
 
 	void Update()
 	{
-		foreach (Transform child in enemyContainer)
-		{
-			Enemy_Wolf asdf = child.GetComponent<Enemy_Wolf>();
-			Debug.Log(asdf.idleSounds[1]);
-		}
+
+	}
+
+	protected static readonly float[] RANDOM_PITCH_RANGE = { 0.9f, 1.1f };
+
+	/// <summary>
+	/// Returns the AudioClip played.
+	/// </summary>
+	public static AudioClip PlayRandomSound(Component gameObject, AudioClip[] sounds)
+	{
+		AudioSource audio = gameObject.GetComponent<AudioSource>();
+
+		int soundIndex = (int)Mathf.Round(Random.value * (sounds.Length - 1));
+		audio.clip = sounds[soundIndex];
+
+		audio.pitch = Random.value * (RANDOM_PITCH_RANGE[1] - RANDOM_PITCH_RANGE[0]) + RANDOM_PITCH_RANGE[0];
+
+		audio.Play();
+		return audio.clip;
+	}
+
+	public static void PlayRandomSoundAtPoint(AudioClip[] sounds, Vector3 point, Transform parent = null)
+	{
+		Transform pointSound;
+		if (parent)
+			pointSound = Instantiate(INSTANCE.pointSoundPrefab, point, Quaternion.identity, parent);
+		else
+			pointSound = Instantiate(INSTANCE.pointSoundPrefab, point, Quaternion.identity);
+
+		AudioClip sound = PlayRandomSound(pointSound, sounds);
+		Destroy(pointSound.gameObject, sound.length);
 	}
 }

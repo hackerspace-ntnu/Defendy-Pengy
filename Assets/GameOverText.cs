528fbf41c1a5703027ca17b110a1072098e2d6ea
Hackerspace VR <hackerspacevr@gmail.com>
2017-10-19 23:42:35 +0200

    Game over message farther away

--- Assets/GameOverText.cs
+++ Assets/GameOverText.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameOverText : MonoBehaviour {
-    private Transform parent;
-    private int index;
+	private Transform parent;
+	private int index;
 	// Use this for initialization
 	void Start () {
-        parent = transform.parent;
-        index = gameObject.transform.GetSiblingIndex();
+		parent = transform.parent;
+		index = gameObject.transform.GetSiblingIndex();
 
-        var vector = new Vector3(2f,0f,0f);
-        vector = Quaternion.Euler(0f, 90f*((float)index), 0f) * vector;
-        transform.position = parent.transform.position + vector;
-        transform.LookAt(parent.transform);
+		var vector = new Vector3(3f,0f,0f);
+		vector = Quaternion.Euler(0f, 90f*((float)index), 0f) * vector;
+		transform.position = parent.transform.position + vector;
+		transform.LookAt(parent.transform);
 	}
 	
 	// Update is called once per frame

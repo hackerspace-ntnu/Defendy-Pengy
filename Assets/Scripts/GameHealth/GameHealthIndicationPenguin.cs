5fd614b2e00e594dc48a4c7a4686fb95759d57ce
Hackerspace VR <hackerspacevr@gmail.com>
2017-11-08 17:40:34 +0100

    Added penguin death sounds

--- Assets/Scripts/GameHealth/GameHealthIndicationPenguin.cs
+++ Assets/Scripts/GameHealth/GameHealthIndicationPenguin.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class GameHealthIndicationPenguin : MonoBehaviour, IGameHealthIndicationItem {
 	private int health = 1;
+    public AudioClip deathSound;
+    private AudioSource audioSource;
 	public int GetHealthAmount() {
 		return health;
 	}
 
 	// Use this for initialization
 	void Start () {
-		
+        audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -22,6 +24,7 @@ public class GameHealthIndicationPenguin : MonoBehaviour, IGameHealthIndicationI
 		//transform.parent.GetComponent<GameHealthManager>().DecreaseGameHealth ((uint)health);
 	}
 	public void Kill(){
+        audioSource.PlayOneShot(deathSound);
 		gameObject.GetComponent<Animator> ().Play ("Die");
 		Destroy (gameObject, 2f);
 	}

5896db8966b330b0101467fc090a38e7eb8de34f
Håkon Vågsether <hauk142@gmail.com>
2017-11-09 13:38:15 +0100

    Added extremely basic cloud spawner scripts

---
+++ Assets/Scripts/Clouds/Cloud.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.Translate (0.05f, 0f, 0f);
+	}
+}
---
+++ Assets/Scripts/Clouds/CloudSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudSpawner : MonoBehaviour {
+	public Cloud[] cloudPrefabs;
+	public int interval;
+	private int framesToNextSpawn = 0;
+
+	void Start () {
+	}
+
+	void Update () {
+		if (framesToNextSpawn <= 0) {
+			Cloud cloud = Instantiate (cloudPrefabs [0].gameObject, transform.position, Quaternion.identity).GetComponent<Cloud> ();
+			cloud.transform.position = new Vector3(Random.Range(-400f, -100f), 80f, Random.Range(-400f, -100f));
+			cloud.transform.eulerAngles = new Vector3(0f, -45f, 0f);
+			cloud.transform.parent = GameObject.Find ("Decorations").transform;
+			framesToNextSpawn += interval;
+		}
+		framesToNextSpawn -= 1;
+		//print (framesToNextSpawn);
+	}
+}

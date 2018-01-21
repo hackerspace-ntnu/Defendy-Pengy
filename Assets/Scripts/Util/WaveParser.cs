b678533ba139fe580e737a670561b3c7d2b1eece
Erlend Skarpnes <erl1sharpy@gmail.com>
2018-01-21 18:17:43 +0100

    Added multiple spawn points

--- Assets/Scripts/Util/WaveParser.cs
+++ Assets/Scripts/Util/WaveParser.cs
@@ -5,61 +5,53 @@ using UnityEngine;
 
 public class WaveParser
 {
+
 	public static List<EnemyWave> ParseWaveFile(TextAsset wavefile)
 	{
 		List<EnemyWave> waves = new List<EnemyWave>();
-
-		List<EnemySpawner.EnemyType> enemies_currentWave = new List<EnemySpawner.EnemyType>();
+        List<int> spawnerID_currentWave = new List<int>();
+        List<EnemySpawner.EnemyType> enemies_currentWave = new List<EnemySpawner.EnemyType>();
 		List<float> delays_currentWave = new List<float>();
 
-		foreach (string l in wavefile.text.Split(new[] { Environment.NewLine }, StringSplitOptions.None))
-		{
-			string line = l.Trim();
-			if (line.Length == 0 || line[0] == '/' || line[0] == '#')
-			{
-				AddWave(waves, enemies_currentWave, delays_currentWave);
-				continue;
-			}
+        foreach (string l in wavefile.text.Split(new[] { Environment.NewLine }, StringSplitOptions.None))
+        {
+            string line = l.Trim();
+            if (line.Length == 0 || line[0] == '/' || line[0] == '#')
+            {
+                AddWave(waves, spawnerID_currentWave, enemies_currentWave, delays_currentWave);
+                continue;
+            }
 
-			string[] tokens = line.Split(' ');
-			switch (tokens.Length)
-			{
-				case 0:
-					break;
+            string[] tokens = line.Split(' ');
 
-				case 2:
-					enemies_currentWave.Add(GetEnemyType(tokens[0]));
-					delays_currentWave.Add(float.Parse(tokens[1]));
-					break;
+            if (tokens.Length != 4)
+                throw new System.FormatException("Invalid enemy type declaration: " + line);
 
-				case 3:
-					int enemyCount = int.Parse(tokens[0]);
-					EnemySpawner.EnemyType enemy = GetEnemyType(tokens[1]);
-					float delay = float.Parse(tokens[2]);
-					for (int i = 0; i < enemyCount; i++)
-					{
-						enemies_currentWave.Add(enemy);
-						delays_currentWave.Add(delay);
-					}
-					break;
-
-				default:
-					throw new System.FormatException("Invalid enemy type declaration: " + line);
-			}
-		}
+            int spawnID = int.Parse(tokens[0]);
+            int enemyCount = int.Parse(tokens[1]);
+            EnemySpawner.EnemyType enemy = GetEnemyType(tokens[2]);
+            float delay = float.Parse(tokens[3]);
+            for (int i = 0; i < enemyCount; i++)
+            {
+                spawnerID_currentWave.Add(spawnID);
+                enemies_currentWave.Add(enemy);
+                delays_currentWave.Add(delay);
+            }
+        }
 
 		if (enemies_currentWave.Count > 0)
-			AddWave(waves, enemies_currentWave, delays_currentWave);
+			AddWave(waves, spawnerID_currentWave, enemies_currentWave, delays_currentWave);
 
 		return waves;
 	}
 
-	private static void AddWave(List<EnemyWave> waves, List<EnemySpawner.EnemyType> enemies_currentWave, List<float> delays_currentWave)
+	private static void AddWave(List<EnemyWave> waves, List<int> spawnerID_currentWave, List<EnemySpawner.EnemyType> enemies_currentWave, List<float> delays_currentWave)
 	{
 		if (enemies_currentWave.Count == 0)
 			return;
 
-		waves.Add(new EnemyWave(enemies_currentWave.ToArray(), delays_currentWave.ToArray()));
+		waves.Add(new EnemyWave(spawnerID_currentWave.ToArray(), enemies_currentWave.ToArray(), delays_currentWave.ToArray()));
+        spawnerID_currentWave.Clear();
 		enemies_currentWave.Clear();
 		delays_currentWave.Clear();
 	}

3c60c0712c2fdfaf4e2d7e46a3bd970219317fe8
Hackerspace VR <hackerspacevr@gmail.com>
2018-01-25 18:52:45 +0100

    Added minilevel to menu, replaced menu and level1 with new menu and level1

--- Assets/Scripts/SpinAround.cs
+++ Assets/Scripts/SpinAround.cs
@@ -11,7 +11,7 @@ public class SpinAround : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = new Vector3 (transform.position.x, 1 + 0.1f * Mathf.Sin (0.2f * Time.time) + 0.7f, transform.position.z);
+		transform.position = new Vector3 (transform.position.x, 0.3f + 0.1f * Mathf.Sin (0.2f * Time.time) + 0.7f, transform.position.z);
 		transform.Rotate (Vector3.up * (10 * Time.deltaTime + 0.08f * Mathf.Sin(Time.time) ));
 	}
 }

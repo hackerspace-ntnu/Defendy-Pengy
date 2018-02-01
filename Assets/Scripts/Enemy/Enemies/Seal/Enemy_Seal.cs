f217873d853d06743dbcc23a26fa7d68776a195e
Håkon Vågsether <haakonsv@gmail.com>
2018-02-01 17:09:35 +0100

    Spawning correct level 2 enemies, fixed seal speed

--- Assets/Scripts/Enemy/Enemies/Seal/Enemy_Seal.cs
+++ Assets/Scripts/Enemy/Enemies/Seal/Enemy_Seal.cs
@@ -2,7 +2,7 @@
 
 public class Enemy_Seal : Enemy
 {
-    private float baseSpeed = 3.5f;
+    private float baseSpeed = 1.8f;
     private float speedRange = 1f;
 
     internal override float setSpeed()

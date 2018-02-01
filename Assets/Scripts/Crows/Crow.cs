cfbde1cffa13f48730c915dd880b7890cd4fc55a
Håkon Vågsether <haakonsv@gmail.com>
2018-02-01 17:01:49 +0100

    Initially added crow scripts and prefabs

---
+++ Assets/Scripts/Crows/Crow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crow : MonoBehaviour {
+	public float speed = 4f;
+	public Transform goal;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float step = speed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards(transform.position, goal.position, step);
+	}
+}

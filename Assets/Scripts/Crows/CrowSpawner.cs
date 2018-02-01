47a6d9dfab21e11db7d85ba156c8dc995943d9dc
Håkon Vågsether <haakonsv@gmail.com>
2018-02-01 20:25:52 +0100

    Added crow sound to level 1

--- Assets/Scripts/Crows/CrowSpawner.cs
+++ Assets/Scripts/Crows/CrowSpawner.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class CrowSpawner : MonoBehaviour {
 	public Crow crowPrefab;
 	public Transform[] goals;
+	private AudioSource audioSource;
+	public AudioClip crowSound;
 	// Use this for initialization
 	void Start () {
-		
+		audioSource = gameObject.GetComponent<AudioSource> ();
 	}
 	
 	// Update is called once per frame
@@ -16,6 +18,7 @@ public class CrowSpawner : MonoBehaviour {
 	}
 
 	public void SpawnCrows() {
+		audioSource.PlayOneShot (crowSound);
 		for (int i = 0; i < goals.Length; i++) {
 			Crow crow = Instantiate (crowPrefab.gameObject, transform.position, Quaternion.identity).GetComponent<Crow> ();
 			crow.transform.eulerAngles = new Vector3 (0f, -45f, 0f);

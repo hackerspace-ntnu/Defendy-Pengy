704a65ad41b6a32f22a3736eff34b1312a06186e
Håkon Vågsether <haakonsv@gmail.com>
2018-02-04 18:30:26 +0100

    Smooth scaling Fireball and Damage by size

--- Assets/MagicBook/Spells/Fireball/Fireball.cs
+++ Assets/MagicBook/Spells/Fireball/Fireball.cs
@@ -4,79 +4,69 @@ using UnityEngine;
 
 public class Fireball : Spell
 {
-	public float speed = 5f;
+    public float speed = 5f;
     public float damage = Mathf.Ceil(200f / 3f); // 200 is the amount of health bears currently have
-	private Vector3 direction;
+    public float maxAdditionalDamage = 100f;
+    public float playerHoldScalingDuration = 3f;
+    private Vector3 direction;
     public Light pointLight;
-    
-	#region ParticleSystem
-	public ParticleSystem ps;
-	private float targetStartSize;
-	private float curStartSize;
-	private float curVelocitySmoothDamp = 0f;
 
-	
-	private float maxScaleMultiplier = 4f; //double the size of fireball;
-	private Vector3 maxScale;
-	private Vector3 scaleSmoothDamp;
+    #region ParticleSystem
+    public ParticleSystem ps;
+    private float targetStartSize;
+    private float curStartSize;
+    private float curVelocitySmoothDamp = 0f;
 
 
-	#endregion
+    private float maxScaleMultiplier = 10f; //double the size of fireball;
+    private Vector3 maxAdditionalScale;
 
-	protected override void Start_Derived()
-	{
-		targetStartSize = ps.main.startSize.constant;
-		curStartSize = 0f;
-		maxScale = transform.lossyScale * maxScaleMultiplier;
+
+    #endregion
+
+    protected override void Start_Derived()
+    {
+        targetStartSize = ps.main.startSize.constant;
+        curStartSize = 0f;
+        maxAdditionalScale = transform.lossyScale * maxScaleMultiplier;
         StartCoroutine(Show());
-	}
+    }
 
-	protected override void Update_Derived()
-	{
-		if (Input.GetKeyDown(KeyCode.Space))
-			HidePreview();
-		if (fired)
-		{
-			transform.Translate(direction * (speed * Time.deltaTime), Space.World);
-			// TODO: edit gravity of fireball to get the moving feel
-		}
-		else
-		{
-			/*if (isInitSizing == true) //make the fireball smoothly increase in size in the beginning
-			{
-				var main = ps.main;
-				//curSize += 1f * Time.deltaTime; // takes 1 sec to become a full ball
-				curStartSize = Mathf.SmoothDamp(curStartSize, targetStartSize, ref curVelocitySmoothDamp, 1f);
-				main.startSize = curStartSize;
-				if (Mathf.Abs(curStartSize - targetStartSize) < 0.1f)
-				{
-					//print("This is equal");
-					isInitSizing = false;
-				}
-			}*/
-		}
-	}
+    protected override void Update_Derived()
+    {
+        if (Input.GetKey(KeyCode.Space))
+            OnPlayerHoldSpell();
+        if (fired)
+        {
+            transform.Translate(direction * (speed * Time.deltaTime), Space.World);
+            // TODO: edit gravity of fireball to get the moving feel
+        }
+    }
 
-	protected override void FixedUpdate_Derived(){}
+    protected override void FixedUpdate_Derived() { }
 
-	public override void OnPlayerHoldSpell()
-	{
-		//move itself towards the hand position
-		if (transform.parent)
-			transform.position = Vector3.Lerp(transform.position, transform.parent.position + transform.parent.forward*0.2f, 0.2f);
-		if (!isInitSizing) //wait till the spell is on a ready size. Then increase the size;
-		{
-			transform.localScale = Vector3.SmoothDamp(transform.localScale, maxScale, ref scaleSmoothDamp, 3f);
-			//add damage
-		}
-		
-	}
-	
-	public override void Fire(Vector3 handDirection)
-	{
-		transform.parent = null;
-		direction = handDirection;
-		fired = true;
+    public override void OnPlayerHoldSpell()
+    {
+        //move itself towards the hand position
+        if (transform.parent)
+            transform.position = Vector3.Lerp(transform.position, transform.parent.position + transform.parent.forward * 0.2f, 0.2f);
+        if (!isInitSizing) //wait till the spell is on a ready size. Then increase the size;
+        {
+            if (transform.lossyScale.x >= 0.5f)
+                return;
+            var a = Time.deltaTime / playerHoldScalingDuration;
+            transform.localScale += maxAdditionalScale * Time.deltaTime * 2f;
+            damage += maxAdditionalDamage * a * 0.65f;
+            print(damage);
+        }
+
+    }
+
+    public override void Fire(Vector3 handDirection)
+    {
+        transform.parent = null;
+        direction = handDirection;
+        fired = true;
         StartCoroutine(LifeTimeOut());
     }
 
@@ -87,33 +77,39 @@ public class Fireball : Spell
         if (!fired)
             return;
         IDamagable damagable = collider.gameObject.GetComponentInParent<IDamagable>();
-        if(damagable != null) {
+        if (damagable != null)
+        {
             damagable.InflictDamage(damage);
             Destroy(gameObject);
-        } else {
+        }
+        else
+        {
             // To ignore collisions with MagicBook's own collider; layer 0 is the Default layer
-            if(collider.gameObject.layer == 0)
+            if (collider.gameObject.layer == 0)
                 Destroy(gameObject);
         }
     }
 
 
 
-    public override void ShowPreview() {
+    public override void ShowPreview()
+    {
         StartCoroutine(Show());
     }
 
     public override void HidePreview()
-	{
-		StartCoroutine(HideAndDestroy());
-	}
-    
+    {
+        StartCoroutine(HideAndDestroy());
+    }
 
-    private IEnumerator HideAndDestroy() { //gradually decrease the size of the spell and finally destroy
+
+    private IEnumerator HideAndDestroy()
+    { //gradually decrease the size of the spell and finally destroy
         Vector3 scaleAtThatMoment = transform.localScale;
         float intensityAtThatMoment = pointLight.intensity;
         float curDuration = fadeDuration;
-        while(curDuration > 0f) {
+        while (curDuration > 0f)
+        {
             var a = Time.deltaTime / fadeDuration;
             transform.localScale -= scaleAtThatMoment * a;
             pointLight.intensity -= intensityAtThatMoment * a;
@@ -124,15 +120,16 @@ public class Fireball : Spell
     }
 
 
-    private IEnumerator Show() { //gradually increase the size of the spell
-        yield return new WaitForSeconds(delayBetweenSpawns);
+    private IEnumerator Show()
+    { //gradually increase the size of the spell
         Vector3 scaleAtThatMoment = transform.localScale;
         float intensityAtThatMoment = pointLight.intensity;
         transform.localScale = Vector3.zero;
         pointLight.intensity = 0f;
         float curDuration = fadeDuration;
-        curStartSize = Mathf.SmoothDamp(curStartSize, targetStartSize, ref curVelocitySmoothDamp, 1f);
-        while(curDuration > 0f) {
+        yield return new WaitForSeconds(delayBetweenSpawns);
+        while (curDuration > 0f)
+        {
             var a = Time.deltaTime / fadeDuration;
             transform.localScale += scaleAtThatMoment * a;
             pointLight.intensity += intensityAtThatMoment * a;
@@ -142,7 +139,8 @@ public class Fireball : Spell
         isInitSizing = false;
     }
 
-    protected IEnumerator LifeTimeOut() {
+    protected IEnumerator LifeTimeOut()
+    {
         yield return new WaitForSeconds(30);
         Destroy(gameObject);
     }

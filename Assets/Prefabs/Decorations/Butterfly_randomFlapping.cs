d843e030bdd0e9d5eddc722d8a02ee1cb3676bc5
Dabble <ddabble@users.noreply.github.com>
2018-02-22 00:27:29 +0100

    Randomized Butterfly flapping speed

---
+++ Assets/Prefabs/Decorations/Butterfly_randomFlapping.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Butterfly_randomFlapping : StateMachineBehaviour
+{
+	public float speedVariabilityPercentage = 5f;
+
+	private int speedMultiplierHash = Animator.StringToHash("SpeedMultiplier");
+
+	public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		// Produces a value between -5 and 5
+		float speedMultiplier_percentage = Random.value * (speedVariabilityPercentage * 2) - speedVariabilityPercentage;
+		// Translates to between 0.95 and 1.05
+		float speedMultiplier = 1f + speedMultiplier_percentage / 100f;
+		animator.SetFloat("SpeedMultiplier", speedMultiplier);
+	}
+}
